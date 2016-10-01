542fc31326480f75becc8fd311a6ae1d5c66c76b
Hugo Arganda <hugo.arganda@gmail.com>
2016-09-30 20:08:16 -0500

    Disabled debug by default

--- SerialCommand.h
+++ SerialCommand.h
@@ -10,7 +10,7 @@
 #include <string.h>
 #include <SoftwareSerial.h>
 
-#define SERIAL_CMD_DBG_EN     1     /* Set this value to 1 to enable debugging */
+#define SERIAL_CMD_DBG_EN     0     /* Set this value to 1 to enable debugging */
 #define SERIAL_CMD_BUFF_LEN  32     /* Max length for each serial command */
 
 /*
